a61b687fd7bba6a43afd61dc96b41ab3b284534a
BankuBa <paaekow04@gmail.com>
2024-06-24 10:21:10 +0000

    rewrote js and made chnges to mobile css

--- code.js
+++ code.js
@@ -1,11 +1,12 @@
 let panels = document.querySelectorAll(`.panel`);
 
-panels.forEach( panel => {
+panels.forEach(panel => {
   panel.addEventListener(`click`, () => {
     removeActive()
     panel.classList.add(`active`)
   })
 })
+
 
 function removeActive() {
   panels.forEach(panel => {
